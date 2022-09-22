a537b56f77cc10a77eb4b1c010fc53a20409742b
zombain1231 <krutyix@mail.ru>
2022-09-22 13:06:13 +0700

    user4

--- user4-1/Form1.cs
+++ user4-1/Form1.cs
@@ -12,9 +12,25 @@ namespace user4_1
 {
     public partial class Form1 : Form
     {
+        Form2 form2 = new Form2();
+        Form3 form3 = new Form3();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form2.Owner = this;
+            form2.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            form3.Owner = this;
+            form3.Show();
+            this.Hide();
         }
     }
 }
